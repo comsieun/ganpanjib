c36c0e24ee2c863150441b1e18fa2ce05161b8f5
comsieun <ja24z8@naver.com>
2024-05-19 15:10:52 +0900

    0519 불행조각 카운트, 장애물 자리 조정 및 색으로 표시

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let score = 0
+let misfortune = 0
+const barrier = [0, 1, 2] //0:구덩이 1:전봇대 2:쓰레기통
+const item = [0, 1] //0:에너지 드링크 1:불행 조각 
 
 //게임시작
 document.addEventListener('DOMContentLoaded',()=>{
+    const start = document.getElementById("scoreHeader")
     const chara = document.getElementById("mainChara")
     const LBTN = document.getElementById("arrowLeft")
     const RBTN = document.getElementById("arrowRight")
 //마우스이벤트 클릭시 게임시작
 chara.addEventListener("click", ()=>{
-    setInterval(updateScore, 300)
+    // setInterval(updateScore, 300) // 0.3초당 스코어증가
+    start.innerHTML = `score: ${score} 불행조각: ${misfortune}`
 
 //키보드이벤트
     let x = 1
-    const block = 33
+    const block = 40
     const print = () => {
         chara.style.left = `${x * block}%`
     }
@@ -47,7 +52,12 @@ RBTN.addEventListener("click", ()=>{ //오른버튼 누른 경우
 })
 //score 증가
 function updateScore(s=1){
-    const start = document.getElementById("startBTN")
+    const start = document.getElementById("scoreHeader")
     score += s
-    start.innerHTML = `score: ${score}`
+    start.innerHTML = `score: ${score} 불행조각: ${misfortune}`
+}
+function updateMisfortune(s=1){
+    const start = document.getElementById("scoreHeader")
+    misfortune += s
+    start.innerHTML = `score: ${score} 불행조각: ${misfortune}`
 }
