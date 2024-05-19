885b613d538c412451ad18df010bbc7362ff2c89
comsieun <ja24z8@naver.com>
2024-05-19 13:32:44 +0900

    0518 화살표클릭이동 방향키이동 스코어증가

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+let score = 0
+
+//게임시작
 document.addEventListener('DOMContentLoaded',()=>{
     const chara = document.getElementById("mainChara")
-//마우스이벤트, 속성변경
+    const LBTN = document.getElementById("arrowLeft")
+    const RBTN = document.getElementById("arrowRight")
+//마우스이벤트 클릭시 게임시작
 chara.addEventListener("click", ()=>{
+    setInterval(updateScore, 300)
+
 //키보드이벤트
-    let x = 2
-    const block = 30
+    let x = 1
+    const block = 33
     const print = () => {
         chara.style.left = `${x * block}%`
     }
     print()
-
+//방향키로 한 경우
     const [left, right] = [37, 39]
     document.body.addEventListener
     (
@@ -20,11 +27,27 @@ chara.addEventListener("click", ()=>{
         x-= 1
         break
     case right:
-        if (x<3) 
+        if (x<2) 
         x += 1
         break
     }
     print()
 })
-    })
+LBTN.addEventListener("click", ()=>{ //왼쪽버튼 누른 경우
+    if (x>0) 
+    x-= 1
+    print()
 })
+RBTN.addEventListener("click", ()=>{ //오른버튼 누른 경우
+    if (x<2) 
+    x += 1
+    print()
+})
+})
+})
+//score 증가
+function updateScore(s=1){
+    const start = document.getElementById("startBTN")
+    score += s
+    start.innerHTML = `score: ${score}`
+}
